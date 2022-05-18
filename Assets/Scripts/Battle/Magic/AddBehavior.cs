038a0d7f217ea6361089fdc42e0428a5197d119f
王振波 <771454644@qq.com>
2022-05-18 19:51:55 +0800

    优化代码

--- Assets/Scripts/Battle/Magic/AddBehavior.cs
+++ Assets/Scripts/Battle/Magic/AddBehavior.cs
@@ -1,12 +1,11 @@
 using BehaviorTree.Battle;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 namespace Battle {
   [CreateAssetMenu(menuName = "效果/加行为树")]
   public class AddBehavior : MagicAction {
     public override bool IgnoreOnEnd => false;
-    public AssetReferenceT<BehaviorGraph> BehaviorGraphRef;
+    public BehaviorGraph BehaviorGraph;
 
     public override void Run(BattleManager battleManager, Context context, EffectArgs args) {
       if(args.IsEnd) {
@@ -14,8 +13,7 @@ namespace Battle {
           battleManager.BehaviorManager.RemoveBehavior(buffContext.BehaviorRuntimeId);
         }
       } else {
-        BehaviorGraph behaviorGraph = BehaviorGraphRef.Asset as BehaviorGraph;
-        int behaviorRuntimeId = battleManager.BehaviorManager.AddBehavior(behaviorGraph, args.Source, args.Target);
+        int behaviorRuntimeId = battleManager.BehaviorManager.AddBehavior(BehaviorGraph, args.Source, args.Target);
         if (behaviorRuntimeId > 0 && context is BuffContext buffContext) {
           buffContext.BehaviorRuntimeId = behaviorRuntimeId;
         }
