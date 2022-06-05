51df0b8ec7275a5d189d06b99cda2b4a5fcf235b
王振波 <771454644@qq.com>
2022-06-05 15:45:10 +0800

    优化代码

--- Assets/Scripts/Util/MathUtil.cs
+++ Assets/Scripts/Util/MathUtil.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
 
 public enum CompareMethod {
   [InspectorName("µÈÓÚ")]
@@ -18,28 +17,12 @@ public enum CompareMethod {
 
 public static class MathUtil {
   public const float EPSILON = 0.0001f;
-  public static readonly Random Random = new Random();
-
-  public static void FisherYatesShuffle<T>(T[] array) {
-    for (int i = array.Length - 1; i > 0; i--) {
-      int index = Random.Next(0, i);
-      (array[index], array[i]) = (array[i], array[index]);
-    }
-  }
 
   public static void FisherYatesShuffle<T>(List<T> list) {
     for (int i = list.Count - 1; i > 0; i--) {
-      int index = Random.Next(0, i);
+      int index = UnityEngine.Random.Range(0, i);
       (list[index], list[i]) = (list[i], list[index]);
     }
-  }
-
-  public static bool Prob(float prob) {
-    return Random.NextDouble() < prob;
-  }
-
-  public static bool Approximately(float f1, float f2) {
-    return Mathf.Abs(f1 - f2) < EPSILON;
   }
 
   public static bool Compare(IComparable left, IComparable right, CompareMethod method) {
