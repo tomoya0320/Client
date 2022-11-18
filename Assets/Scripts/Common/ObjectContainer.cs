6bcc923e475f666d8b64af52eb781ac288548956
wangzhenbo <771454644@qq.com>
2022-11-18 10:17:00 +0800

    对象引用脚本

---
+++ Assets/Scripts/Common/ObjectContainer.cs
@@ -0,0 +1,26 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCore {
+  public class ObjectContainer : SerializedMonoBehaviour {
+    [SerializeField, DictionaryDrawerSettings(KeyLabel = "名称", ValueLabel = "对象引用")]
+    private Dictionary<string, GameObject> ObjectDict;
+
+    public GameObject GetGameObject(string objName) {
+      if (string.IsNullOrEmpty(objName)) {
+        return null;
+      }
+      ObjectDict.TryGetValue(objName, out var obj);
+      return obj;
+    }
+    
+    public Transform GetTransform(string objName) {
+      return GetGameObject(objName)?.transform;
+    }
+
+    public T GetComponent<T>(string objName) where T : Component {
+      return GetGameObject(objName)?.GetComponent<T>();
+    }
+  }
+}
